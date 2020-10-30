7b9d20a7e46f99c05719506f8b8c69645945eb5c
sandy13521 <nandavarmukund1999@gmail.com>
2020-10-30 20:35:04 +0530

    Initial Commit

--- app/src/main/java/com/example/networkingapp/MainActivity.java
+++ app/src/main/java/com/example/networkingapp/MainActivity.java
@@ -8,12 +8,12 @@ import android.provider.Settings;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
-
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationCompat;
-
 import com.google.android.material.snackbar.Snackbar;
+import java.io.DataOutputStream;
+import java.net.Socket;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -31,6 +31,8 @@ public class MainActivity extends AppCompatActivity {
         enableButton = findViewById(R.id.enable);
         startButton = findViewById(R.id.start);
         createButton = findViewById(R.id.create);
+
+        //Enables the the app to listen to the notifications.
         enableButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -38,38 +40,39 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        // Turns on Bluetooth when start button is clicked.
         startButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-                if (bluetoothAdapter == null) {
-                    Snackbar.make(findViewById(R.id.main), "Bluetooth is not supported in your Phone", Snackbar.LENGTH_LONG).show();
-                }
-                else if (!bluetoothAdapter.isEnabled()) {
-                    Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-                    startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
-                    Intent discoverableIntent =
-                            new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
-                    discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 300);
-                    startActivity(discoverableIntent);
-                }
+            BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+            if (bluetoothAdapter == null) {
+                Snackbar.make(findViewById(R.id.main), "Bluetooth is not supported in your Phone", Snackbar.LENGTH_LONG).show();
+            }
+            else if (!bluetoothAdapter.isEnabled()) {
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+                Intent discoverableIntent =
+                        new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+                discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 300);
+                startActivity(discoverableIntent);
+            }
             }
         });
 
         createButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                NotificationManager nManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-                NotificationCompat.Builder ncomp = new NotificationCompat.Builder(MainActivity.this);
-                ncomp.setContentTitle("My Notification");
-                ncomp.setContentText("Notification Listener Service Example");
-                ncomp.setTicker("My Notification Tests");
-                ncomp.setSmallIcon(R.drawable.ic_launcher_background);
-                ncomp.setAutoCancel(true);
-                nManager.notify((int) System.currentTimeMillis(), ncomp.build());
+            NotificationManager nManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+            NotificationCompat.Builder ncomp = new NotificationCompat.Builder(MainActivity.this);
+            ncomp.setContentTitle("My Notification");
+            ncomp.setContentText("Notification Listener Service Example");
+            ncomp.setTicker("My Notification Tests");
+            ncomp.setSmallIcon(R.drawable.ic_launcher_background);
+            ncomp.setAutoCancel(true);
+            nManager.notify((int) System.currentTimeMillis(), ncomp.build());
             }
         });
-
+        startService(new Intent(this,NotificationListener.class));
         // Starting this Service to resolve issues like NotificationListenerService Getting Disconnected automatically.
         startService(new Intent(this, NotificationCollectorMonitorService.class));
     }
@@ -84,4 +87,9 @@ public class MainActivity extends AppCompatActivity {
             Snackbar.make(findViewById(R.id.main), "You need to turn on Bluetooth to use this app.", Snackbar.LENGTH_INDEFINITE).show();
         }
     }
+
+     @Override protected void onStop(){
+        super.onStop();
+         Toast.makeText(MainActivity.this,"Application is being closed.", Toast.LENGTH_SHORT).show();
+     }
 }
--- app/src/main/java/com/example/networkingapp/NotificationReceiver.java
+++ app/src/main/java/com/example/networkingapp/NotificationReceiver.java
@@ -3,25 +3,34 @@ package com.example.networkingapp;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.StrictMode;
 import android.util.Log;
 
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.Socket;
 import java.util.Objects;
 
 public class NotificationReceiver extends BroadcastReceiver {
     public Socket socket;
     public DataOutputStream dataOutputStream;
+    public BufferedReader bufferedReader;
 
     public NotificationReceiver() {
         try {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
+                    .permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+            Log.i("NOtificationReceiver","Constructor called");
             Thread thread = new Thread(new Runnable() {
                 @Override
                 public void run() {
                     try {
-                        socket = new Socket("192.168.43.22", 3000);
+                        socket = new Socket("192.168.43.105", 3000);
                         dataOutputStream = new DataOutputStream(socket.getOutputStream());
+                        bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
                         Log.i("Socket", "Created");
                     } catch (IOException e) {
                         e.printStackTrace();
@@ -39,14 +48,16 @@ public class NotificationReceiver extends BroadcastReceiver {
         if (Objects.equals(intent.getAction(), "com.notification")) {
             try {
                 if (dataOutputStream!=null){
-                    dataOutputStream.writeChars(intent.getStringExtra("text"));
+                    dataOutputStream.writeChars("HELLO WORLD");
+                    String x = bufferedReader.readLine();
+                    Log.i("Content ACK",x);
+//                    if ()
+                    Log.i("Notification Content",intent.getStringExtra("text"));
                     Log.i("Notification Received", "Notification Sent");
-
                 }
                 else{
 
                 }
-
             } catch (Exception e) {
                 e.printStackTrace();
             }
